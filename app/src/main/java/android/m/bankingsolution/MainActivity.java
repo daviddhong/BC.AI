b9d98465596a9772ac633f0b061faca95a0aa5f0
edwardkim <edwardk.kim@alumni.ubc.ca>
2020-01-11 15:45:00 -0800

    Bottom navigation view

--- app/src/main/java/android/m/bankingsolution/MainActivity.java
+++ app/src/main/java/android/m/bankingsolution/MainActivity.java
@@ -1,8 +1,15 @@
 package android.m.bankingsolution;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.m.bankingsolution.Fragments.BotFragment;
+import android.m.bankingsolution.Fragments.ContactsFragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new BotFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.chat:
+                            fragment = new BotFragment();
+                            break;
+
+                        case R.id.contacts:
+                            fragment = new ContactsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            fragment).commit();
+
+                    return true;
+                }
+            };
 }
