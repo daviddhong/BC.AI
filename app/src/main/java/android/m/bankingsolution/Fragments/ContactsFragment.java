1b1c46d9f03e8210006c64e4a65a3038db2bcd70
david hong <davidhong022@gmail.com>
2020-01-12 02:49:48 -0800

    camera, retrieve photo data from firebase

--- app/src/main/java/android/m/bankingsolution/Fragments/ContactsFragment.java
+++ app/src/main/java/android/m/bankingsolution/Fragments/ContactsFragment.java
@@ -1,7 +1,10 @@
 package android.m.bankingsolution.Fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.m.bankingsolution.ActivityLoginStart.InitialStartingActivity;
+import android.m.bankingsolution.Camera.ImageActivity;
 import android.m.bankingsolution.R;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,18 +12,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ContactsFragment extends Fragment {
     private View myCardView;
     private Button SignOut;
     private FirebaseAuth auth;
     private ImageView mybcimage;
+    private String currentUID;
 
     @Nullable
     @Override
@@ -38,6 +47,7 @@ public class ContactsFragment extends Fragment {
     private void initiateFields() {
         SignOut = myCardView.findViewById(R.id.SignOutButton);
         auth = FirebaseAuth.getInstance();
+        currentUID = auth.getUid();
         mybcimage = myCardView.findViewById(R.id.mybusinesscardimageview);
     }
 
@@ -54,7 +64,33 @@ public class ContactsFragment extends Fragment {
     }
 
     private void retrieveMyBusinessCard() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
 
+        // [START upload_create_reference]
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+
+        StorageReference islandRef = storageRef.child("images/" + currentUID + ".jpg");
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data for "images/island.jpg" is returns, use this as needed
+//                Toast.makeText(getContext(), "Here is your Business Card", Toast.LENGTH_LONG).show();
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0 , bytes.length);
+                mybcimage.setImageBitmap(bitmap);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Toast.makeText(getActivity(), "Failed to find image", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 
 }
